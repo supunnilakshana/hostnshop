dcff564aaae5cd367884f6dee0020d908fdb589a
supunnilakshana <supunnilakshana1999@gmail.com>
2025-01-20 00:36:21 +0530

    fix : page structure setup

--- hostnshop/src/app/admin/home/page.tsx
+++ hostnshop/src/app/admin/home/page.tsx
@@ -1,11 +1,10 @@
-import Dashboard from '@/presentation/components/dashboard'
-import React from 'react'
+import Dashboard from "@/presentation/pages/dashboard/dashboard";
+import React from "react";
 
 export default function page() {
   return (
     <div>
-        <Dashboard/>
-      
+      <Dashboard />
     </div>
-  )
+  );
 }
--- hostnshop/src/presentation/pages/dashboard/dashboard.tsx
+++ hostnshop/src/presentation/pages/dashboard/dashboard.tsx
@@ -1,15 +1,15 @@
 "use client";
 
+import Footer from "@/presentation/components/footer";
 import React from "react";
-import Footer from "./footer";
 
 export default function Dashboard() {
   return (
     <div>
       <div className="px-5 bg-bg_primary text-white text-xl py-8 relative md:h-[120px] h-auto ">
         Dashboard
-       </div>
-       <Footer />
+      </div>
+      <Footer />
     </div>
   );
 }
