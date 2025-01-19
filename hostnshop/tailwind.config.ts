b57b43f06a7df79c93752cb8245de779aedeed9f
Dinithirathnayaka <dinithinayanamali98@gmail.com>
2025-01-19 14:34:10 +0530

    feat:modifies dashbaord

--- hostnshop/tailwind.config.ts
+++ hostnshop/tailwind.config.ts
@@ -1,18 +1,36 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	"./src/presentation/**/*.{js,ts,jsx,tsx,mdx}",
+	"./src/app/admin/**/*.{js,ts,jsx,tsx,mdx}",
+	
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+  	extend: {
+		colors: {
+			bg_secondary: "#CCB3FF", 
+			bg_primary: "#8A4FFF", 
+			accent: "#FFFFFF", // White for backgrounds
+			grayLight: "#EDEDED", // Light gray for subtle highlights
+			sidebarBg: "#F7F9FC", // Background of the sidebar
+			cardBg: "#FFFFFF", // Background for cards
+			textPrimary: "#1E293B", // Dark text for primary headings
+			textSecondary: "#475569", // Lighter gray for secondary text
+			link: "#2563EB", // Blue color for links
+			btn_hover: "#7733ff"	  },
+
+
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
   plugins: [],
 } satisfies Config;
